23068fff7aed7de635f094673952a6a1ee129554
Tim <tim.bauwens1@kahosl.be>
2014-03-06 18:36:09 +0100

    mainactivity clickevents aangemaakt

--- HulpdienstenApp/gen/com/example/hulpdienstenapp/R.java
+++ HulpdienstenApp/gen/com/example/hulpdienstenapp/R.java
@@ -14,12 +14,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Button01=0x7f060003;
-        public static final int Button02=0x7f060002;
+        public static final int ButtonCall=0x7f060002;
+        public static final int ButtonHelp=0x7f060005;
         public static final int Frame1=0x7f060000;
-        public static final int button1=0x7f060001;
-        public static final int button2=0x7f060005;
-        public static final int button3=0x7f060004;
+        public static final int buttonHowto=0x7f060003;
+        public static final int buttonReport=0x7f060001;
+        public static final int buttonSettings=0x7f060004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- HulpdienstenApp/src/com/example/hulpdienstenapp/MainActivity.java
+++ HulpdienstenApp/src/com/example/hulpdienstenapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.hulpdienstenapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
@@ -11,5 +14,58 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        createButtonListeners();
     }
+
+    //creates all the event listeners for the buttons on the main activity
+	private void createButtonListeners() {
+		
+		Button buttonHowto = (Button) findViewById(R.id.buttonHowto);
+		Button buttonSettings = (Button) findViewById(R.id.buttonSettings);
+		Button buttonReport = (Button) findViewById(R.id.buttonReport);
+		Button ButtonCall = (Button) findViewById(R.id.ButtonCall);
+		Button ButtonHelp = (Button) findViewById(R.id.ButtonHelp);
+
+		buttonHowto.setOnClickListener(new OnClickListener()
+	    {
+	      public void onClick(View v)
+	      {
+
+	      }
+	    });
+		
+		buttonSettings.setOnClickListener(new OnClickListener()
+	    {
+	      public void onClick(View v)
+	      {
+
+	      }
+	    });
+		
+		buttonReport.setOnClickListener(new OnClickListener()
+	    {
+	      public void onClick(View v)
+	      {
+
+	      }
+	    });
+		
+		ButtonCall.setOnClickListener(new OnClickListener()
+	    {
+	      public void onClick(View v)
+	      {
+
+	      }
+	    });
+		
+		ButtonHelp.setOnClickListener(new OnClickListener()
+	    {
+	      public void onClick(View v)
+	      {
+
+	      }
+	    });
+		
+	}
 }
